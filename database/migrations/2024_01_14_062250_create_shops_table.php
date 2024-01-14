5e5f6676ef10c26791b68882f86d7c67a54859a5
ever711 <talentdev725@gmail.com>
2024-01-13 22:30:31 -0800

    add shop table with model and controller

---
+++ database/migrations/2024_01_14_062250_create_shops_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+			$table->integer('id', true);
+			$table->string('name', 191);
+			$table->string('email', 191)->nullable();
+			$table->string('phone', 191)->nullable();
+			$table->string('country', 191)->nullable();
+			$table->timestamps(6);
+			$table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
